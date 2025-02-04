908b73665a42f4e6cd4cded3950724dd080efe89
nelson <nelson.penagosj@gmail.com>
2025-02-04 12:09:12 -0500

    CONFIG_PORT variable was added

--- users/src/app.py
+++ users/src/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from db import db
 from config import Config
@@ -17,4 +18,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    port = int(os.getenv("CONFIG_PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port)
